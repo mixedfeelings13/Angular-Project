87db44f6a83738756a94d638de404a358d9fd8dc
mixedfeelings13 <fuegodjh@gmail.com>
2024-04-01 00:53:56 +0100

    Add FiltersComponent to app.module.ts and update home.component.html and home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   cols = 3;
+  category: string | undefined;
 
   constructor() { }
 
@@ -15,6 +16,10 @@ export class HomeComponent implements OnInit {
 
   onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
   }
 
 }
